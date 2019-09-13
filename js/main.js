1f7323052459dcff7f682747c576e35a5c865c4e
TeIxe <teixetawrex@gmail.com>
2019-09-14 00:14:00 +0300

    add slider in header

--- js/main.js
+++ js/main.js
@@ -1,9 +1,17 @@
 jQuery(document).ready(function() {
+	$('.header-owl').owlCarousel({
+		items: 1,
+		nav: false,
+		loop: true
+	});
 	$('.logo').addClass('logo-start');
-	$('.science').addClass('fadeInLeftBig')
+	$('.science').addClass('fadeInLeft')
 	$('.search i').addClass('fadeIn')
 	$('.ham').on('click', function(event) {
 		$('body').toggleClass('activeMenu');
 		$('.search').toggleClass('fadeOut');
 	});
+	if (/Android|webOS|iPhone|iPad|iPod|BlackBerry|BB|PlayBook|IEMobile|Windows Phone|Kindle|Silk|Opera Mini/i.test(navigator.userAgent)) {
+	    $('body').toggleClass('active-phone');
+	}
 });
