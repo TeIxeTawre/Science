8d175cee46a352b37187ad1fd1286d42534d02d1
TeIxe <teixetawrex@gmail.com>
2019-09-18 21:39:20 +0300

    edit main screen

--- js/main.js
+++ js/main.js
@@ -4,20 +4,33 @@ jQuery(document).ready(function() {
 	$('.header-owl').owlCarousel({
 		items: 1,
 		nav: false,
-		loop: true
+		loop: false
 	});
 
+	var handler = function(){
+		$('.science').removeEventListener('animationend', handler);
+	};
+
 	$('.logo').addClass('logo-start');
-	$('.science').addClass('fadeInLeft animated')
-	$('nav a i').addClass('fadeIn animated')
 	$('.ham').on('click', function(event) {
 		$('body').toggleClass('activeMenu');
 		$('.search').toggleClass('fadeOut animated');
 	});
-	$('.menu-a').addClass('fadeIn animated')
-	$('.bright .window a').toggleClass('fadeIn animated');
-	$('.bright .window .window-pair p').toggleClass('fadeInDown animated');
 
+
+	$('.science').addClass('fadeInLeft animated');
+	$('nav a i').addClass('fadeIn animated');
+	$('.menu-a').addClass('fadeIn animated');
+	$('.bright .window a').addClass('fadeIn animated');
+	$('.bright .window .window-pair p').addClass('fadeInDown animated');
+
+	setTimeout(function(){
+		$('.science').removeClass('fadeInLeft animated');
+		$('nav a i').removeClass('fadeIn animated');
+		$('.menu-a').removeClass('fadeIn animated');
+		$('.bright .window a').removeClass('fadeIn animated');
+		$('.bright .window .window-pair p').removeClass('fadeInDown animated');
+	}, 2000);
 
 	if (/Android|webOS|iPhone|iPad|iPod|BlackBerry|BB|PlayBook|IEMobile|Windows Phone|Kindle|Silk|Opera Mini/i.test(navigator.userAgent))
 		$('body').addClass('active-phone');
@@ -54,3 +67,12 @@ jQuery(document).ready(function() {
 
 	
 });
+
+
+function ref(fn) {
+	window.requestAnimationFrame(function(){
+		window.requestAnimationFrame(function(){
+			fn();
+		});
+	});
+}
