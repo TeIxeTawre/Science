4dd9acddf25d35eb7f630d52eb08eb8798dc486e
TeIxe <teixetawrex@gmail.com>
2019-09-18 10:37:44 +0300

    edit main screen

--- js/main.js
+++ js/main.js
@@ -1,4 +1,6 @@
 jQuery(document).ready(function() {
+	var popUser = false;
+
 	$('.header-owl').owlCarousel({
 		items: 1,
 		nav: false,
@@ -12,13 +14,7 @@ jQuery(document).ready(function() {
 		$('body').toggleClass('activeMenu');
 		$('.search').toggleClass('fadeOut');
 	});
-
-	$('.btn').hover(function() {
-		$(this).removeClass('animated');
-		$(this).addClass('pulse animated');
-	}, function() {
-		$(this).removeClass('pulse');
-	});
+	$('.menu-a').addClass('fadeIn')
 
 	if (/Android|webOS|iPhone|iPad|iPod|BlackBerry|BB|PlayBook|IEMobile|Windows Phone|Kindle|Silk|Opera Mini/i.test(navigator.userAgent))
 		$('body').addClass('active-phone');
@@ -29,4 +25,29 @@ jQuery(document).ready(function() {
 			$('body').removeClass('active-phone');
 		}
 	});
+
+	$('body').on('click', function() {
+		if (popUser == false) {
+			$('.popap-active').removeClass('popap-active');
+		}
+	});
+
+	$('.user').on('click', function(event) {
+		event.preventDefault();
+		$('.popap-user').toggleClass('popap-active');
+	});
+
+	$('.popap-user').hover(function() {
+		popUser = true;
+	}, function() {
+		popUser = false;
+	});
+
+	$('.user').hover(function() {
+		popUser = true;
+	}, function() {
+		popUser = false;
+	});
+
+	
 });
